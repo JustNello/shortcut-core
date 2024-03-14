2ed4dfc25101d08ba6231a2b04575aa5803b5cdd
Luca.IACOMINO <luca.iacomino@akkodis.com>
2024-03-14 10:21:16 +0100

    First commit

---
+++ src/test/java/com/nellocorp/shortcut/ShortcutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nellocorp.shortcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortcutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
